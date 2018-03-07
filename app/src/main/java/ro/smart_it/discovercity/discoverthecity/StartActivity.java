53b806600c5bc98c9a5c42e162e8e2879762fcd7
Marius David <mariusda@yahoo.com>
2018-03-07 09:49:46 +0200

    Initial Commit

---
+++ app/src/main/java/ro/smart_it/discovercity/discoverthecity/StartActivity.java
@@ -0,0 +1,13 @@
+package ro.smart_it.discovercity.discoverthecity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
